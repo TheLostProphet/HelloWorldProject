3e44a00d07ba2eb0a57068c0b1b076d41fdf3a5e
Klindbeck <35391608+TheLostProphet@users.noreply.github.com>
2024-11-10 16:41:37 -0700

    Add files via upload

---
+++ Helloworld.py
@@ -0,0 +1,21 @@
+#Let User Input a Value
+name = input("What is your Name? " )
+
+#removes whitespace from string
+name = name.strip()
+
+#capitilize output of each "word" or name
+name= name.title()
+
+#Print hello + Input Value
+print("Hello, " + name)
+
+#can also be done as -> print("hello, ", name)
+
+
+#Can also be done like:
+#print("Hello, ", end="")
+#print(name)
+
+
+# by 찻주전자
---
+++ HelloworldUpdated.py
@@ -0,0 +1,15 @@
+#Let User Input a Value
+name = input("What is your Name? " ).strip().title()
+
+#Print hello + Input Value
+print("Hello, " + name)
+
+#can also be done as -> print("hello, ", name)
+
+
+#Can also be done like:
+#print("Hello, ", end="")
+#print(name)
+
+
+# by 찻주전자
